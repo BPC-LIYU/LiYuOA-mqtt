5b546da99d06bbe680d2e79e60ed37bdf3998f53
fanjunwei <fanjunwei003@163.com>
2016-04-30 09:25:16 +0800

    mongodb 接口修改

--- service/dao/mongo_service.js
+++ service/dao/mongo_service.js
@@ -20,16 +20,45 @@ function mongoConnect() {
 var insertOne = exports.insertOne = function (table, obj) {
     return mongoConnect().then(function (db) {
         var collection = db.collection(table);
-        return Q.nfcall(collection.insertOne.bind(collection), obj);
+        var result = Q.nfcall(collection.insertOne.bind(collection), obj);
+        result.then(function () {
+            db.close();
+        }, function () {
+            db.close();
+        });
+        return result;
     });
 };
 var find = exports.find = function (table, query) {
     query = query || {};
     return mongoConnect().then(function (db) {
-        return db.collection(table).find(query);
+        var defered = Q.defer();
+        var cursor = db.collection(table).find(query);
+        var result = cursor.toArray();
+        result.then(function (result) {
+            defered.resolve(result);
+            db.close();
+        }, function (error) {
+            defered.reject(error);
+            db.close();
+        });
+        return defered.promise;
     });
 };
-
+var update = exports.update = function (table, query, set_obj) {
+    query = query || {};
+    set_obj = set_obj || {};
+    return mongoConnect().then(function (db) {
+        var collection = db.collection(table);
+        var result = Q.nfcall(collection.updateOne.bind(collection), query, {"$set": set_obj});
+        result.then(function () {
+            db.close();
+        }, function () {
+            db.close();
+        });
+        return result;
+    });
+};
 exports.addMessage = function (message) {
     return insertOne('message', message);
 };
